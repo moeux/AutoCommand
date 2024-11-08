56300906ce2049f15888af0ef6ac3a79024491e1
moeux <68925026+moeux@users.noreply.github.com>
2024-11-08 22:50:36 +0100

    Added documentation.

--- AutoCommand/Utils/EnumParser.cs
+++ AutoCommand/Utils/EnumParser.cs
@@ -1,7 +1,20 @@
 namespace AutoCommand.Utils;
 
+/// <summary>
+///     Utility class to parse enum values from strings
+/// </summary>
 public static class EnumParser
 {
+    /// <summary>
+    ///     Parses a <see cref="string" /> to the desired enum type
+    /// </summary>
+    /// <param name="value">The value to parse</param>
+    /// <param name="ignoreCase">Whether to ignore case when parsing, <c>true</c> by default</param>
+    /// <typeparam name="T">Enum type to parse to</typeparam>
+    /// <returns>
+    ///     An instance of <c>T</c> if the value could be parsed, <c>null</c> otherwise or if the value is <c>null</c> or
+    ///     empty
+    /// </returns>
     public static T? ParseToEnumValue<T>(string? value, bool ignoreCase = true) where T : struct
     {
         if (string.IsNullOrWhiteSpace(value))
@@ -13,6 +26,13 @@ public static class EnumParser
         return null;
     }
 
+    /// <summary>
+    ///     Parses multiple <see cref="string" />s to the desired enum types
+    /// </summary>
+    /// <param name="values">The values to parse</param>
+    /// <param name="ignoreCase">Whether to ignore case when parsing, <c>true</c> by default</param>
+    /// <returns>An instance of <see cref="List{T}" /> containing all values which could be parsed</returns>
+    /// <remarks>If values is <c>null</c> this method returns an empty <see cref="List{T}" /></remarks>
     public static List<T> ParseToEnumValues<T>(IEnumerable<string>? values, bool ignoreCase = true) where T : struct
     {
         List<T> enumValues = [];
--- AutoCommand/Utils/EnvironmentUtils.cs
+++ AutoCommand/Utils/EnvironmentUtils.cs
@@ -1,7 +1,16 @@
 namespace AutoCommand.Utils;
 
+/// <summary>
+///     Utility class to retrieve environment variables
+/// </summary>
 public static class EnvironmentUtils
 {
+    /// <summary>
+    ///     Retrieves the environment variable with an optional fallback value
+    /// </summary>
+    /// <param name="name">The environment variable to retrieve</param>
+    /// <param name="fallback">The fallback value</param>
+    /// <returns>The environment variable or the fallback value if the environment variable is empty</returns>
     public static string GetVariable(string name, string fallback = "")
     {
         var value = Environment.GetEnvironmentVariable(name);
